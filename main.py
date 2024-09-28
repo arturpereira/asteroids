ce761a63d3127889dd0ba4047e5628303d873d9b
arturpereira <arturcp@gmail.com>
2024-09-27 23:22:24 -0300

    clock and color change

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    
+    dt = 0
     
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
     
@@ -19,10 +19,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill((0, 0, 0))
+        screen.fill("black")
         player.draw(screen)
         
         pygame.display.flip()
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
